685573c759ee224b652a879d540b425d547c2cb5
wangqi <wangqi@chicv.com>
2024-12-04 11:38:49 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import {
+		onLaunch,
+	} from '@dcloudio/uni-app';
+
+	onLaunch((option) => {
+		console.log(option, 'onLaunch')
+	})
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
